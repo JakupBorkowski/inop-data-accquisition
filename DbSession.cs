5daeee60ea753ca534476e418ec2c55e67a0e3ef
JakupBorkowski <jborkowski2801@gmail.com>
2022-08-29 21:02:44 +0200

    123

--- DbSession.cs
+++ DbSession.cs
@@ -15,7 +15,7 @@ namespace USB_205_DataAccquisition
         {
            // string sql = "datasource=localhost;port=3306;username=root;password=;database=usb205db";
             //string sql = "datasource=database-1.crdizhrpr8gd.us-east-1.rds.amazonaws.com;port=3306;username=root;password=12345678;database=datebase1;Convert Zero Datetime=True";
-
+            int damian_liczba_zebow = 2;
             MySqlConnection conn = new MySqlConnection(Globals.sql);//////////
             try
             {
